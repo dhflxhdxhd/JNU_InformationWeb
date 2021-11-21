060e2ea865f0f68e275954f8492691f6908d7307
김아현 <dhflxhdxhd@gmail.com>
2021-11-21 21:38:36 +0900

    react-map

---
+++ test/src/map.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import { useEffect } from 'react';
+
+// const mapContainer = () => {
+//     return (
+//         <div class="mapContainer" style={{
+//             width : '500px',
+//             height : '500px'
+//         }}></div>
+//     );
+// }
+
+const { kakao } = window;
+const MapContainer = () => {
+    
+    useEffect(() => {
+        const container = document.getElementById('myMap');
+		const options = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			level: 3
+		};
+        const map = new kakao.maps.Map(container, options);
+    }, []);
+
+    return (
+        <div id='myMap' style={{
+            width: '500px', 
+            height: '500px'
+        }}></div>
+    );
+}
+export default MapContainer
