c186393ba99d7e089117c743ad077f9f9dc5bc20
김아현 <dhflxhdxhd@gmail.com>
2021-11-21 22:17:22 +0900

    modify map latLng

--- test/src/map.js
+++ test/src/map.js
@@ -7,7 +7,7 @@ const MapContainer = () => {
     useEffect(() => {
         const container = document.getElementById('myMap');
 		const options = {
-			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			center: new kakao.maps.LatLng(33.45606028280052, 126.56205448172588),
 			level: 3
 		};
         const map = new kakao.maps.Map(container, options);
