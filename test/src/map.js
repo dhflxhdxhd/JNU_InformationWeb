b33a1a1ff274cb4979f3c07a0ba6287fbabc1d53
김아현 <dhflxhdxhd@gmail.com>
2021-11-21 22:25:31 +0900

    create map.js

---
+++ test/src/map.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+const { kakao } = window;
+const MapContainer = () => {
+    
+    useEffect(() => {
+        const container = document.getElementById('myMap');
+		const options = {
+			center: new kakao.maps.LatLng(33.45606028280052, 126.56205448172588),
+			level: 3
+		};
+        const map = new kakao.maps.Map(container, options);
+    }, []);
+
+    return (
+        <div id='myMap'></div>
+    );
+}
+export default MapContainer
