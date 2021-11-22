7bb1cd4ada79e40b2bade93ce7ffa5c8bc34f571
김아현 <dhflxhdxhd@gmail.com>
2021-11-22 09:53:47 +0900

    create erdiin app

---
+++ erdiin/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import MapContainer from './map';
+
+function App() {  
+  return (
+    <div className="wrapper">
+      <MapContainer />
+    </div>
+  );
+}
+
+export default App;
--- test/src/map.js
+++ test/src/map.js
@@ -11,6 +11,62 @@ const MapContainer = () => {
 			level: 3
 		};
         const map = new kakao.maps.Map(container, options);
+
+        const busPosition_A = [
+            {
+                title:'정문',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'약학대학',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'학생회관',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'인문대학(서)',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'학생생활관',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'인문대학(동)',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'중앙도서관',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'의학전문대학원',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'공과대학4호관',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'해양과학대학4호관',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'교양강의동',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'본관',
+                latlng: new.kakao.maps.LatLng()
+            },
+            {
+                title:'해양과학대학1호관',
+                latlng: new.kakao.maps.LatLng()
+            }
+            
+        ]
     }, []);
 
     return (
