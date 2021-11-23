a309a905bfd667510c406f380121b7c192d1f6a9
김아현 <dhflxhdxhd@gmail.com>
2021-11-23 11:29:32 +0900

    Update busMap.js

--- erdiin/src/busMap.js
+++ erdiin/src/busMap.js
@@ -37,8 +37,11 @@ const busMap = () =>{
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-    
+    const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+
+
+    let marker = 
     positionA.forEach(element => {
     new kakao.maps.Marker(
         {
@@ -50,8 +53,6 @@ const busMap = () =>{
     );
     });
 
-
-
     positionB.forEach(element => {
     new kakao.maps.Marker(
         {
@@ -62,6 +63,6 @@ const busMap = () =>{
     );
     });
 
-    
+    return marker;
 }
 export default MapContainer
