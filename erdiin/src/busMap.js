f6b318c85d2055f1e560bc332169100c7132c24b
김아현 <dhflxhdxhd@gmail.com>
2021-11-23 11:54:22 +0900

    marker 한개 이미지 변경
    not complete
    testing

--- erdiin/src/busMap.js
+++ erdiin/src/busMap.js
@@ -16,13 +16,7 @@ const MapContainer = () => {
 }
 
 
-// // maker -> busimg. 수정수정수정수정
-// const imgSrc = "../img/bus-stop.png"
 
-// const createMarkerImg = (src,size,options) => {
-//     const busImg = new kakao.maps.MarkerImage(src,size,options);
-//     return busImg
-// }
 const busMap = () =>{
     const container = document.getElementById('myMap');
     const options = {
@@ -31,38 +25,40 @@ const busMap = () =>{
     };
     const map = new kakao.maps.Map(container, options);
 
+    const busImgSrc = '../img/bus.png', 
+      busImgSize = new kakao.maps.Size(64, 69),
+      busImgOption = {offset: new kakao.maps.Point(27, 69)}; 
 
-    const imageSrc = "../img/bus-stop.png", // 마커이미지의 주소입니다    
-    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-      
-    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-    const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+    const markerImage = new kakao.maps.MarkerImage(busImgSrc,busImgSize,busImgOption);
+    const markerPos = new kakao.maps.LatLng(33.459757645661135,126.56156244046907);
 
+    const marker = new kakao.maps.Marker({
+        position : markerPos,
+        image : markerImage
+    })
 
+    marker.setMap(map)
+    // positionA.forEach(el=> {
+    // new kakao.maps.Marker(
+    //     {
+    //         map:map,
+    //         position: new kakao.maps.LatLng(el.lat,el.lng),
+    //         title:el.title,
+    //         img:markerImage
+    //     }
+    // );
+    // });
 
-    let marker = 
-    positionA.forEach(element => {
-    new kakao.maps.Marker(
-        {
-            map:map,
-            position: new kakao.maps.LatLng(element.lat,element.lng),
-            title:element.title,
-            img:markerImage
-        }
-    );
-    });
+    // positionB.forEach(el => {
+    // new kakao.maps.Marker(
+    //     {
+    //         map:map,
+    //         position: new kakao.maps.LatLng(el.lat,el.lng),
+    //         title:el.title,
+    //         img:markerImage
+    //     }
+    // );
+    // });
 
-    positionB.forEach(element => {
-    new kakao.maps.Marker(
-        {
-            map:map,
-            position: new kakao.maps.LatLng(element.lat,element.lng),
-            title:element.title,
-        }
-    );
-    });
-
-    return marker;
 }
 export default MapContainer
