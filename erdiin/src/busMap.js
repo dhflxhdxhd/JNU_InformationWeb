f6034ac7ea51a4d44bd12f61f95ba9b2ee79da14
김아현 <dhflxhdxhd@gmail.com>
2021-11-25 11:34:56 +0900

    positionA marker image change

--- erdiin/src/busMap.js
+++ erdiin/src/busMap.js
@@ -10,8 +10,9 @@ let map;
 const MapContainer = () => {
     
     useEffect(() => {
-        busMap();
-        
+
+        busMarker();
+
         facLoc.createOfficeMarkers();
         facLoc.createStoreMarkers();
         facLoc.createRstrtMarkers();
@@ -43,7 +44,18 @@ const MapContainer = () => {
 }
 
 
-const busMap = () =>{
+// const createMap = () => {
+//     const container = document.getElementById('myMap');
+//     const options = {
+//     center: new kakao.maps.LatLng(33.45606028280052, 126.56205448172588),
+//     level: 3
+//     };
+//     const map = new kakao.maps.Map(container, options);
+    
+//     return map;
+// }
+
+const busMarker = () =>{
     const container = document.getElementById('myMap');
     const options = {
     center: new kakao.maps.LatLng(33.45606028280052, 126.56205448172588),
@@ -51,25 +63,46 @@ const busMap = () =>{
     };
     const map = new kakao.maps.Map(container, options);
 
-    positionA.forEach(element => {
-    new kakao.maps.Marker(
-        {
-            map:map,
-            position: new kakao.maps.LatLng(element.lat,element.lng),
-            title:element.title,
-        }
-    );
-    });
+    var imageSrc = "https://github.com/dhflxhdxhd/JNU_InformationWeb/blob/main/img/bus-stop.png?raw=true"; 
 
-    positionB.forEach(element => {
-    new kakao.maps.Marker(
-        {
-            map:map,
-            position: new kakao.maps.LatLng(element.lat,element.lng),
-            title:element.title,
-        }
-    );
-    });
+     // 마커 이미지의 이미지 크기 입니다
+     const imageSize = new kakao.maps.Size(40,40); 
+    
+     // 마커 이미지를 생성합니다    
+     const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+    
+    for (let i = 0; i < positionA.length; i ++) {
+        let latlng = new kakao.maps.LatLng(positionA[i].lat,positionA[i].lng);
+        let title = positionA[i].title
+        // 마커를 생성합니다
+        let marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: latlng, // 마커를 표시할 위치
+            title : title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image : markerImage // 마커 이미지 
+        });
+
+
+}
+    // positionA.forEach(element => {
+    // new kakao.maps.Marker(
+    //     {
+    //         map:map,
+    //         position: new kakao.maps.LatLng(element.lat,element.lng),
+    //         title:element.title,
+    //     }
+    // );
+    // });
+
+    // positionB.forEach(element => {
+    // new kakao.maps.Marker(
+    //     {
+    //         map:map,
+    //         position: new kakao.maps.LatLng(element.lat,element.lng),
+    //         title:element.title,
+    //     }
+    // );
+    // });
 }
 
 export default MapContainer
