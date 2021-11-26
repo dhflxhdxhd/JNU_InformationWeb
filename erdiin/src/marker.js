f860a3ef2d80c27fad6cc26f3e197dd41000f0e1
김아현 <dhflxhdxhd@gmail.com>
2021-11-26 14:22:19 +0900

    marker all 주석
    신경쓰지마셈

--- erdiin/src/marker.js
+++ erdiin/src/marker.js
@@ -1,49 +1,49 @@
-import { positionA , positionB } from "./positions";
+// import { positionA , positionB } from "./positions";
 
-const { kakao } = window;
-let busMarkers = [];
+// const { kakao } = window;
+// let busMarkers = [];
 
 
-const createMarkerImage = (src,size) => {
-    const markerImage = new kakao.maps.markerImage(src,size);
-    return markerImage;
-}
+// const createMarkerImage = (src,size) => {
+//     const markerImage = new kakao.maps.markerImage(src,size);
+//     return markerImage;
+// }
 
-const createMarker = (position,image) => {
-    let marker = new kakao.maps.Marker({
-        position : position,
-        image : image
-    });
-    return marker;
-}
+// const createMarker = (position,image) => {
+//     let marker = new kakao.maps.Marker({
+//         position : position,
+//         image : image
+//     });
+//     return marker;
+// }
 
-export const createBusMarkers = () => {
+// export const createBusMarkers = () => {
     
-    const imageSrc_A = "https://github.com/dhflxhdxhd/JNU_InformationWeb/blob/main/img/bus-stop.png?raw=true"; 
-    const imageSize = new kakao.maps.size(40,40);
+//     const imageSrc_A = "https://github.com/dhflxhdxhd/JNU_InformationWeb/blob/main/img/bus-stop.png?raw=true"; 
+//     const imageSize = new kakao.maps.size(40,40);
 
-    for(let i = 0 ; i<positionA.length; i++){    
+//     for(let i = 0 ; i<positionA.length; i++){    
 
-        let latlng = new kakao.maps.LatLng(positionA[i].lat,positionA[i].lng);
-        let markerImage = createMarkerImage(imageSrc_A,imageSize);
-        let marker = createMarker(latlng,markerImage);
+//         let latlng = new kakao.maps.LatLng(positionA[i].lat,positionA[i].lng);
+//         let markerImage = createMarkerImage(imageSrc_A,imageSize);
+//         let marker = createMarker(latlng,markerImage);
 
-        busMarkers.push(marker);
-    }
-}
+//         busMarkers.push(marker);
+//     }
+// }
 
-const setBusMarkers = (map) => {
-    for(let i=0; i<busMarkers.length; i++){
-        busMarkers[i].setMap(map);
-    }
-}
+// const setBusMarkers = (map) => {
+//     for(let i=0; i<busMarkers.length; i++){
+//         busMarkers[i].setMap(map);
+//     }
+// }
 
 
-export const changeMarker = (t,map) => {
-    const busBtn = document.getElementById('busBtn');
+// export const changeMarker = (t,map) => {
+//     const busBtn = document.getElementById('busBtn');
 
-    if (t === 'busBtn'){
-        setBusMarkers(map);
-    }
-}
+//     if (t === 'busBtn'){
+//         setBusMarkers(map);
+//     }
+// }
 
