be703867ecc1d615cca782813fdf80f02a178e75
김아현 <dhflxhdxhd@gmail.com>
2021-12-01 15:19:37 +0900

    get nowtime
    npm moment

--- erdiin/src/App.js
+++ erdiin/src/App.js
@@ -1,15 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
 import MapContainer from './busMap';
+import './busMap.css'
 // import navlist from './nav'
+import * as time from './time'
+
+
 
 function App() {  
   return (
-    <div className="wrapper">
-
+    <div id="wrapper">
+      <div id="navlist">
+        <div>들어갔나?</div>
+      </div>
       <MapContainer />
-
     </div>
+
+
   );
 }
 
---
+++ erdiin/src/time.js
@@ -0,0 +1,7 @@
+import React from "react";
+import moment from 'moment';
+import 'moment/locale/ko';
+
+const nowTime = moment().format('HH:mm:ss');;
+console.log(nowTime);
+
