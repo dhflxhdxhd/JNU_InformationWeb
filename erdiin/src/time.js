cf46c88db49773e8726235bf97ccb4af99c19830
김아현 <dhflxhdxhd@gmail.com>
2021-12-04 23:14:11 +0900

    time data update

--- erdiin/src/time.js
+++ erdiin/src/time.js
@@ -1,15 +1,7 @@
 
-import { A1, A2 } from './bustime'
+import { A1} from './bustime'
 
-const A = [A1,A2];
 
-//현재시간 기준으로 timeA?의 ?값에 따른 time 데이터 설정
-// A버스 B버스 구분.
-//marker 클릭 > time_A <- 해당 marker title 위치 time 데이터 
-//nowtime <- 현재시간
-// cal <- time_A - nowtime 
-// cal이 0보다 크면 > 해당 차이값 = 도착하기까지 남은 시간
-// cal이 0보다 작으면 > ?+1 해서 차이 구함 = 도착하기까지 남은 시간 
 export const callTime = () => {
 
     // let now = new Date();
@@ -24,8 +16,9 @@ export const callTime = () => {
     // let busTime = '08:05';
     
     for (let i=0; i<2; i++){
-        let busTime = A[i][0].time;
+        let busTime = A1[0].time[i];
         console.log("bustime: "+ busTime);
+
         busTime= busTime.split(':');
         let busHours = busTime[0];
         let busMin = busTime[1];
