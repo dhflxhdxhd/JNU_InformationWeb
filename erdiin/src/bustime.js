f28710169eff11b02e463919041661aa169c866b
김아현 <dhflxhdxhd@gmail.com>
2021-12-04 21:49:15 +0900

    bus time에서 값 가져오기

--- erdiin/src/bustime.js
+++ erdiin/src/bustime.js
@@ -1,4 +1,4 @@
-export const timeA1 = [
+export const A1 = [
     {
         title:'정문',
         time: '08:05'
@@ -54,4 +54,58 @@ export const timeA1 = [
     }
 ]
 
-export default timeA1
+export const A2 = [
+    {
+        title:'정문',
+        time: '08:25'
+    },
+    {
+        title:'약학대학',
+        time: '08:26'  
+    },
+    {
+        title:'해양과학대학1호관',
+        time: '08:27'
+    },
+    {
+        title:'본관',
+        time: '08:28'
+    },
+    {
+        title:'학생회관',
+        time: '08:29'
+    },
+    {
+        title:'인문대학(서)',
+        time: '08:30'
+    },
+    {
+        title:'학생생활관',
+        time: '08:31'
+    },
+    {
+        title:'인문대학(동)',
+        time: '08:32'
+    },
+    {
+        title:'중앙도서관',
+        time: '08:33'
+    },
+    {
+        title:'의학전문대학원',
+        time: '08:34'
+    },
+    {
+        title:'공과대학4호관',
+        time: '08:35'
+    }
+    ,
+    {
+        title:'교양강의동',
+        time: '08:36'
+    },
+    {
+        title:'해양과학대학4호관',
+        // time: ''
+    }
+]
--- erdiin/src/time.js
+++ erdiin/src/time.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { timeA1 } from './bustime'
 
+import { A1, A2 } from './bustime'
 
+const A = [A1,A2];
 
 //현재시간 기준으로 timeA?의 ?값에 따른 time 데이터 설정
 // A버스 B버스 구분.
@@ -11,26 +11,35 @@ import { timeA1 } from './bustime'
 // cal이 0보다 크면 > 해당 차이값 = 도착하기까지 남은 시간
 // cal이 0보다 작으면 > ?+1 해서 차이 구함 = 도착하기까지 남은 시간 
 export const callTime = () => {
+
     // let now = new Date();
     // //현재 시간
-    // let hours = now.getHours();
+    // let hours_s = now.getHours();
     // let min = now.getMinutes();
+
     let hours = 8;
-    let min = 0;
+    let min = 10;
 
     //비교 시간
     // let busTime = '08:05';
-    let busTime = timeA1[0].time;
-    console.log("bustime: "+ busTime);
-    busTime= busTime.split(':');
-    let busHours = busTime[0];
-    let busMin = busTime[1];
+    
+    for (let i=0; i<2; i++){
+        let busTime = A[i][0].time;
+        console.log("bustime: "+ busTime);
+        busTime= busTime.split(':');
+        let busHours = busTime[0];
+        let busMin = busTime[1];
 
-    const nowTime = new Date(2020,12,4,hours,min,0);
-    const comTime = new Date(2020,12,4,busHours,busMin,0);
+        const nowTime = new Date(2020,12,4,hours,min,0);
+        const comTime = new Date(2020,12,4,busHours,busMin,0);
 
-    const gap = comTime.getTime() - nowTime.getTime();
-    console.log(Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60)));
+        const gap = comTime.getTime() - nowTime.getTime();
+
+        if (gap > 0){
+            console.log(Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60)));
+            break;
+        }
+    }   
 
 }
 
